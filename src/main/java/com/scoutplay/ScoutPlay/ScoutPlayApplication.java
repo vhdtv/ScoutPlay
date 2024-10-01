7f88b2961fbddac93f317465f477dfe3347f0237
Victor Dias <vhdtv14@gmail.com>
2024-10-01 19:32:34 -0300

    Criação no Spring Initializr do  projeto Scout Play

---
+++ src/main/java/com/scoutplay/ScoutPlay/ScoutPlayApplication.java
@@ -0,0 +1,13 @@
+package com.scoutplay.ScoutPlay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoutPlayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScoutPlayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scoutplay/ScoutPlay/ScoutPlayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scoutplay.ScoutPlay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoutPlayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
