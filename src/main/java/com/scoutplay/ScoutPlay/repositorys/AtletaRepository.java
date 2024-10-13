ef55907632b5c68dd3daa60e89e288a6de8554a3
Victor Dias <vhdtv14@gmail.com>
2024-10-13 16:17:34 -0300

    feat: adição do backend para a tela de login

--- src/main/java/com/scoutplay/ScoutPlay/repositorys/AtletaRepository.java
+++ src/main/java/com/scoutplay/ScoutPlay/repositorys/AtletaRepository.java
@@ -4,9 +4,11 @@ import com.scoutplay.ScoutPlay.models.Atleta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 
 
 @Repository
 public interface AtletaRepository extends JpaRepository<Atleta, String> {
 
+    Optional<Atleta> findByEmail(String email);
 }
--- src/main/java/com/scoutplay/ScoutPlay/repositorys/OlheiroRepository.java
+++ src/main/java/com/scoutplay/ScoutPlay/repositorys/OlheiroRepository.java
@@ -4,6 +4,9 @@ import com.scoutplay.ScoutPlay.models.Olheiro;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface OlheiroRepository extends JpaRepository<Olheiro, String> {
+    Optional<Olheiro> findByEmail(String email);
 }
