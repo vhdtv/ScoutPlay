d47e3ea8268838296efa1d7d8fe842f948e83854
ocai0 <caioalvesfernandes@gmail.com>
2026-05-01 18:56:41 -0300

    refactor: updated input style actions

--- src/front-end/src/components/ui/Input.tsx
+++ src/front-end/src/components/ui/Input.tsx
@@ -6,12 +6,12 @@ export default function Input({fieldName, hideLabel, ...props}: IExtendedProps) 
       <label className="w-full">
         {
           fieldName && (hideLabel != true) 
-            ? <p className="uppercase font-extrabold mb-1 text-indigo-900">
+            ? <p className="uppercase font-extrabold mb-1 text-indigo-900 cursor-pointer">
                 { fieldName || "" }
               </p>
             : null
         }
-        <input type={props.type ?? "text"} placeholder={props.placeholder} className={`py-3 px-5 border-1 border-gray-200 rounded-md placeholder-gray-300 text-lg w-full ${props.className}`} name={fieldName} />
+        <input type={props.type ?? "text"} placeholder={props.placeholder} className={`py-3 px-5 border-1 border-gray-200 focus:outline-none focus:border-slate-400 rounded-md placeholder-gray-300 focus:bg-slate-100 text-lg w-full ${props.className}`} name={fieldName} />
       </label>
     </>
   )
