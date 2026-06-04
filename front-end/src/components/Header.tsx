7a8532f38f25619549990451a0184e3e5490c75d
ocai0 <caioalvesfernandes@gmail.com>
2026-06-04 02:16:11 -0300

    header now redirects if user isn't logged in

--- front-end/src/components/Header.tsx
+++ front-end/src/components/Header.tsx
@@ -1,4 +1,5 @@
-import { Link } from '@tanstack/react-router'
+import { Link, useNavigate } from '@tanstack/react-router'
+import { useEffect, useState } from 'react'
 
 export default function Header() {
   return (
@@ -11,10 +12,20 @@ export default function Header() {
 }
 
 export function LoggedHeader() {
+  const [username, setUsername] = useState("");
+  const navigate = useNavigate();
+  useEffect(() => {
+    const username = localStorage.getItem("username");
+    if(!username || username == "") {
+      navigate({to: "/login"})
+      return
+    }
+    setUsername(username)
+  }, [])
   return (
     <header className='flex justify-end items-center bg-slate-100 border-b-1 border-slate-300 w-full max-h-16'>
       <Link to="/feed" className='px-6 py-3 text-2xs hover:bg-slate-200 cursor-pointer'>Feed</Link>
-      <Link to="/user/$user" params={{user: "me"}} className='px-6 py-3 text-2xs hover:bg-slate-200 cursor-pointer'>Perfil</Link>
+      <Link to="/user/$user" params={{user: username}} className='px-6 py-3 text-2xs hover:bg-slate-200 cursor-pointer'>Perfil</Link>
       <Link to="/settings" className='px-6 py-3 text-2xs hover:bg-slate-200 cursor-pointer'>Configurações</Link>
     </header>
   )
