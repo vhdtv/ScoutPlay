ad16d0838e7529cad70478c2feb5887a8e87c736
ocai0 <caioalvesfernandes@gmail.com>
2026-06-08 11:12:05 -0300

    fix: return type of XUsuarioTipoContaRepository

--- back-end/src/main/java/com/scoutplay/ScoutPlay/repositories/XUsuarioTipoContaRepository.java
+++ back-end/src/main/java/com/scoutplay/ScoutPlay/repositories/XUsuarioTipoContaRepository.java
@@ -4,11 +4,12 @@ import com.scoutplay.ScoutPlay.models.TipoConta;
 import com.scoutplay.ScoutPlay.models.Usuario;
 import com.scoutplay.ScoutPlay.models.XUsuarioTipoConta;
 
+import java.util.Set;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 public interface XUsuarioTipoContaRepository extends JpaRepository<XUsuarioTipoConta, Integer> {
-    XUsuarioTipoConta getByTipoConta(TipoConta tipoConta);
-    XUsuarioTipoConta getByUsuario(Usuario usuario);
-    XUsuarioTipoConta getByAliasId(UUID alias_id);
+    Set<XUsuarioTipoConta> getByTipoConta(TipoConta tipoConta);
+    Set<XUsuarioTipoConta> getByUsuario(Usuario usuario);
+    Set<XUsuarioTipoConta> getByAliasId(UUID alias_id);
 }
